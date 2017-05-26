3a33caec099d0be6536752988789733b719cfebf
Michael Shennan <m.shennan22@gmail.com>
2017-05-26 11:16:22 +0100

    localhost is running

---
+++ seeds.js
@@ -0,0 +1,17 @@
+use path_finder;
+db.dropDatabase();
+
+db.walks.insert([
+  {
+    name: "Royal Mile",
+    start: "Edinburgh Castle",
+    finish: "Holyrood Palace",
+    completed: false
+  },
+  {
+    name: "Meadows",
+    start: "Hope Park Terrace",
+    finish: "The Golf Tavern",
+    completed: true
+  }
+]);
