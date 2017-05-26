79499db457d22de9a3a66e843e6aff477c594e35
Ian <iantennyson@gmail.com>
2017-05-26 16:59:45 +0100

    minor changes to seeds

---
+++ locationSeeds.js
@@ -0,0 +1,21 @@
+use path_finder;
+db.dropDatabase();
+
+db.locations.insert([
+  {
+    name: "Edinburgh Castle",
+    latlng: {lat: 55.9533, lng: -3.1883}
+  },
+  {
+    name: "Hope Park Terrace",
+    latlng: {lat: 10, lng: -10}
+  }
+]);
+
+
+
+// mongo < seeds.js
+// mongo
+// use path_finder
+// db.locations.find()
+// db.locations.find()[0]
