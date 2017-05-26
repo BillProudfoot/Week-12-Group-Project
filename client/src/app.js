a79f21c965b1679cd85c42baece7a8d378e91f76
Ian <iantennyson@gmail.com>
2017-05-26 14:59:23 +0100

    written populate drop method and load map

--- client/src/app.js
+++ client/src/app.js
@@ -4,17 +4,35 @@ var mapWrapper = require('./mapWrapper.js')
 
 
 var app = function() {
+  loadMap();
+  // populateStartDropDown()
+  // populateFinishDropDown()
+  
+  console.log('Map is loading')
+}
+
+
+var loadMap = function(){
   var center = {lat: 55.9533, lng: -3.1883};
-  var zoom = 1;
+  var zoom = 10;
   var mapDiv = document.getElementById("main-map");
-  var startMarker = document.getElementById('option')
 
   var mainMap = new mapWrapper(mapDiv, center, zoom);
-  console.log(startMarker.value)
-  mainMap.addMarker(startMarker.value)
-  mainMap.addMarker(center)
 
-  console.log('Map is loading')
+  
+}
+
+//THIS WILL CALL THE MONGO DB AND POPULATE START DROP DOWN WITH OUR CHOSEN START LOCATION NAMES
+
+var populateStartDropDown = function(locations) {
+  var select = document.querySelector('#start');
+
+  locations.forEach(function(location, index){
+    location.index = index;
+    var option = document.createElement('option');
+    option.text = location.name;
+    select.appendChild(option)
+  })
 }
 
 window.addEventListener("load", app);
