ab8b7cfca4df0dcc5d865bf1a00276cbf817ba78
hatwell <caroline.hatwell@gmail.com>
2017-05-28 15:31:43 +0100

    wrote request functions for walks model

---
+++ client/src/models/walks.js
@@ -0,0 +1,33 @@
+var Walk = require('./walk.js');
+
+var Walks = function() {
+
+}
+
+Walks.prototype = {
+  makeRequest: function(url, callback) {
+    var request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.addEventListener('load', function() {
+      if (request.status !== 200) return;
+      var jsonString = request.responseText;
+      var resultsObject = JSON.parse(jsonString);
+      callback(resultsObject);
+    });
+    request.send();
+  },
+
+  makePostRequest: function (url, callback, payload) {
+    var request = new XMLHttpRequest();
+    request.open('POST', url);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.addEventListener('load', function() {
+      if (request.status !== 200) return;
+      var jsonString = request.responseText;
+      var resultsObject = JSON.parse(jsonString);
+      callback(resultsObject);
+    })
+    console.log(payload);
+    request.send(payload);
+  }
+}
