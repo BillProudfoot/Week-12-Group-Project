6a7bb644fee8d6fcba1658a2532988fa08edc172
hatwell <caroline.hatwell@gmail.com>
2017-05-28 14:32:01 +0100

    refactored wishlistquery to be called walksquery as this should handle all walks not just those on wishlist

--- db/walksQuery.js
+++ db/walksQuery.js
@@ -1,15 +1,15 @@
 var MongoClient = require('mongodb').MongoClient;
 
-var WishlistQuery = function() {
+var WalksQuery = function() {
   this.url = 'mongodb://localhost:27017/path_finder'
 }
 
-WishlistQuery.prototype = {
+WalksQuery.prototype = {
 
   add:function(walkToAdd, onQueryFinished) {
     MongoClient.connect(this.url, function(err,db){
       if (db) {
-        var collection = db.collection("wishlist");
+        var collection = db.collection("walks");
         console.log(walkToAdd);
         collection.insert(walkToAdd);
         collection.find().toArray(function(err, docs){
