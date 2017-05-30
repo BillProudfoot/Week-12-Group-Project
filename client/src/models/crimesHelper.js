93a413c1e76d7daa6585f8762a4c7c84698eeed1
hatwell <caroline.hatwell@gmail.com>
2017-05-30 13:23:56 +0100

    wrote url and average method on crimehelper class

--- client/src/models/crimesHelper.js
+++ client/src/models/crimesHelper.js
@@ -1,2 +1,24 @@
 var Crime = require ('./crime.js');
 var restCrimes = require('./restCrimes.js');
+
+var CrimeHelper = function() {
+
+}
+
+CrimeHelper.protoype = {
+  coordinateFinder: function(coord1, coord2){
+    var avgLat = (coord1.lat + coord2.lat)/2;
+    var avgLng = (coord1.lng + coord2.lng)/2;
+
+    var midpoint = {
+      lat: avgLat,
+      lng: avgLng
+    }
+    return midpoint;
+  }
+
+  urlGenerator: function(coords){
+    var baseUrl = 'https://data.police.uk/api/crimes-street/all-crime?lat=' + coords.lat + '&lng=' + coords.lng + '&date=2017-03';
+    return baseUrl;
+  }
+}
