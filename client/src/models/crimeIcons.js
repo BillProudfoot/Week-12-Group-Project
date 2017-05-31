27b97724969ce26f9711d516bc412ce38d057c97
Ian <iantennyson@gmail.com>
2017-05-31 16:50:31 +0100

    new images added

--- client/src/models/crimeIcons.js
+++ client/src/models/crimeIcons.js
@@ -3,7 +3,7 @@ var CrimeIcons = function(){
   this.crimePics = {
       'anti-social-behaviour': '../img/drugsFinished.png',
       'burgulary': '../img/drugsFinished.png',
-      'criminal-damage-arson': '../img/drugsFinished.png',
+      'criminal-damage-arson': '../img/arsonFinished.png',
       'drugs': 'http://localhost:3000/img/drugsFinished.png',
       'other-theft': '../img/drugsFinished.png',
       'possession-of-weapons': '../img/drugsFinished.png',
