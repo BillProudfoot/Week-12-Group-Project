516a03bcece64731ef8605f47787db8e120333c2
hatwell <caroline.hatwell@gmail.com>
2017-05-31 13:07:49 +0100

    added innerhtml to completed button

--- client/src/views/ui.js
+++ client/src/views/ui.js
@@ -205,7 +205,7 @@ UI.prototype = {
         var completedButton = document.createElement("button");
         completedButton.value = JSON.stringify(walk);
         completedButton.classList.add("btn", "completed");
-        completedButton.innerText = "completed!";
+        completedButton.innerHTML = "<i class='icon-circle-arrow-right icon-large'></i>";
 
         //adds functionality to button where when it is clicked the walk
         // is marked as completed
