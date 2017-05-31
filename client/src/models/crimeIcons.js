f79d195df32f9f036faa0aea4500bf7f04c55698
Ian <iantennyson@gmail.com>
2017-05-31 18:16:45 +0100

    added more images

--- client/src/models/crimeIcons.js
+++ client/src/models/crimeIcons.js
@@ -5,10 +5,10 @@ var CrimeIcons = function(){
       'burgulary': '../img/burgularyFinished.png',
       'criminal-damage-arson': '../img/arsonFinished.png',
       'drugs': 'http://localhost:3000/img/drugsFinished.png',
-      // 'other-theft': '',
+      'other-theft': 'http://localhost:3000/img/otherTheftFinished.png',
       'possession-of-weapons': '../img/weaponFinished.png',
-      // 'public-order': '',
-      // 'shoplifting': '',
+      'public-order': '../img/publicOrderFinished.png',
+      'shoplifting': '../img/shopliftingFinished.png',
       // 'vehicle-crime': '',
       'violent-crime': '../img/crimeSceneFinished.png',
     }
