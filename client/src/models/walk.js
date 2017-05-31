6e2be7a47f3a2fdfa089a3cdb4fcb8afbb79f5cd
hatwell <caroline.hatwell@gmail.com>
2017-05-31 16:24:09 +0100

    tweaks

--- client/src/models/walk.js
+++ client/src/models/walk.js
@@ -1,8 +1,11 @@
 var Walk = function(options){
+  
   this.name = options.name;
   this.start = options.start;
   this.finish = options.finish;
   this.completed = options.completed;
+  this.startlatlng = options.startlatlng;
+  this.finishlatlng = options.finishlatlng;
 }
 
 module.exports = Walk;
